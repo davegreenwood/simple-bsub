cdffa8b823ddd0ddb3f7c3e2d7ac2d850ce22373
Dave Greenwood <david.greenwood@uea.ac.uk>
2019-02-19 18:08:24 +0000

    cmd module add

--- submit.py
+++ submit.py
@@ -12,9 +12,12 @@ from subprocess import run
 cmd = [
     'bsub',
     '-q', 'short-ib',
-    '-J', '"subtest"',
+    '-J', 'subtest',
     '-oo', 'P-subtest.out',
     '-eo', 'P-subtest.err',
+    ". /etc/profile",
+    "module add python/anaconda/2018.12/3.6",
+    "source activate submit",
     'simple', 'arg1', 'arg2', 'arg3'
 ]
 
